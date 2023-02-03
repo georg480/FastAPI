c93c4f2e91fb22cf2552392742367b27a4247723
georg480 <georg.dahmen@gmx.de>
2023-02-03 14:43:13 +0100

    Initial commit
    Signed-off-by: georg480 <georg.dahmen@gmx.de>

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
 
 app = FastAPI()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host= "127.0.0.1", port=99)
-
+    uvicorn.run(app, host="127.0.0.1", port=99)
